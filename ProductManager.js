7c8e25b62da6647193bba2ced192ba7765200d49
Angel San Luis <94204485+falchon100@users.noreply.github.com>
2023-04-30 17:13:04 -0300

    entrega

--- ProductManager.js
+++ ProductManager.js
@@ -5,17 +5,22 @@ class ProductManager{
     constructor(){
     this.products = [];
     this.path="./DesafioClase2.txt";
+    this.idNumber=[];
     }
+
+async readProducts (){
+    let producto= await fs.promises.readFile(this.path,"utf-8")
+    return JSON.parse(producto)
+}
+
+
 // metodo para agregar productos y validacion si es que el codigo ya esta ingresado
     async addProduct(title,description,price,thumbnail,code,stock){
-     
-  // fs.promises.writeFile 
         if (!title||!description||!price||!thumbnail||!code||!stock){
-        console.log('\n No se pudo agregar el producto. Debera completar todos los campos');
-        }
+        console.log('\n No se pudo agregar el producto. Debera completar todos los campos')}
         else{
             let nuevoProducto = {
-                id:this.idGenerator(),
+                id:await this.idGenerator(),
                 title,
                 description,
                 price,
@@ -23,31 +28,39 @@ class ProductManager{
                 code,
                 stock
             }
-            if (this.products.find(ele=>ele.code==code)){
+            this.idNumber.push(nuevoProducto)
+            let producto= await this.readProducts()
+
+            if (producto.find(ele=>ele.code==code)){
                 console.log(`\n No se pudo agrega el producto,ya que el codigo "${code}" ya ha sido ingresado`);
             }else{
             this.products.push(nuevoProducto)
-            fs.promises.writeFile(this.path,JSON.stringify(this.products))
+           await fs.promises.writeFile(this.path,JSON.stringify(this.products))
             }
         }
     }
 
+    
 
 // Genero un id autoincrementable con el largo del producto
-    idGenerator(){
-        return this.products.length + 1;
+   async idGenerator(){
+    let producto= await this.readProducts()  
+    let idNumber = this.idNumber
+    let resultado = producto.length+ idNumber.length+1;
+    return resultado;
     }
+
 //Metodo para Mostrar los productos actuales
-    getProduct(){
-        //fs.promises.readFile
-        fs.promises.readFile(this.path,"utf-8")
-        console.log("\n ---------------Lista De Productos-----------------")
-        return this.products
+   async getProduct(){
+    let producto = await this.readProducts()
+    return console.log(producto);
     }
 
 // Busco en el array de productos si hay un producto con esa id y lo devuelvo o sino Not Found
-    getProductById(id){
-        let producto = this.products.find(elem=>elem.id == id)
+  async  getProductById(id){
+    let producto2= await fs.promises.readFile(this.path,"utf-8")
+    let productoEncontrado = JSON.parse(producto2)
+        let producto = productoEncontrado.find(elem=>elem.id == id)
        if (producto){
         console.log("\n --------------El producto encontrado es :------------");
         console.log(producto);
@@ -56,33 +69,28 @@ class ProductManager{
        }
     }  
     
-    
     updateProduct(){
 
     }
 
-    deleteProduct(e){
-     this.products.forEach((elemento,index)=>{
-        if(elemento.code==e){
-            this.products.splice(index,1)
-            console.log(`se elimino "${e}" de la lista`);
+   async deleteProduct(id){
+    let producto2= await fs.promises.readFile(this.path,"utf-8")
+    let productoEncontrado = JSON.parse(producto2)
+    productoEncontrado.forEach((elemento,index)=>{
+        if(elemento.id==id){
+            productoEncontrado.splice(index,1)
+            console.log(`se elimino "${id}" de la lista`);
         }
      })
+   await fs.promises.writeFile(this.path,JSON.stringify(productoEncontrado))
     }
 }
 
 // Se crea la instancia ProductManager
 let producto= new ProductManager()
 
-//Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
-console.log(producto.getProduct())
-producto.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25)
-console.log(producto.getProduct());
-producto.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25)
-producto.getProductById(1)
-producto.getProductById(3)
-console.log(producto.getProduct());
-/* producto.deleteProduct("abc123")
-console.log(producto.getProduct()); */
-producto.deleteProduct("abc1df23")
-console.log(producto.getProduct())
+producto.addProduct('uno',"uno",1,"uno","uno",1);
+producto.addProduct('dos',"dos",2,"dos","dos",2);
+producto.addProduct('tres',"tres",3,"tres","tres",3);
+
+producto.getProduct()
